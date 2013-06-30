ef1fa386e15631a940a3e29085eb2ffc779ca35d
Roberto Chavez <r_a_chavez@yahoo.com>
2013-06-30 14:44:16 -0700

    swapped mysql for mysqli functions in validateUser.php, cleaned out old mysql code in newUser.php

--- php/newUser.php
+++ php/newUser.php
@@ -1,30 +1,18 @@
 <?php
 	session_start();
 	require_once('config.php');
-		
-/*	
-	$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
-	if(!$link) {
-		error_log('Failed to connect to server: ' . mysql_error());
-    exit;
-	}
-	$db = mysql_select_db(DB_DATABASE);
-	if(!$db) {
-		error_log("Unable to select database");
-    exit;
-	}
-*/	
+
 	//Function to sanitize values received from the form. 
   //Prevents SQL injection
 	function clean($str) {
 		$str = @trim($str);
-		return mysql_real_escape_string($str);
+		return mysqli_real_escape_string($str);
 	}
 
 	$link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
 	if ( !$link ) {
 		error_log('Failed to connect to server: ' . mysqli_connect_error());
-		die( 'Connect error: (' . mysqli_connect_errorno() . ') ' . mysqli_connect_error() );
+		die( 'Connect error: (' . mysqli_connect_errno() . ') ' . mysqli_connect_error() );
 		exit; // just in case
 	}
 	
@@ -38,7 +26,6 @@
 	//Check for duplicate login ID
   $qry1 = "SELECT * FROM players WHERE login='$login'";
 	$result1 = mysqli_query( $link, $qry1 );
-//	$result1 = mysql_query($qry1);
 	if($result1) {
 		if(mysqli_num_rows($result1) > 0) { // duplicate name!
       echo 'duplicate';
@@ -54,7 +41,6 @@
 	$qry = "INSERT INTO players SET firstname='$fname', lastname='$lname',
           email='$email', login='$login', passwd='$passwrd'";
 	$result = @mysqli_query( $link, $qry );
-//	$result = @mysql_query($qry);
 	if($result) {   // Was the query successful
 		echo 'success';
 	}else {
--- php/validateUser.php
+++ php/validateUser.php
@@ -3,25 +3,18 @@
 	session_start();
 	require_once('config.php');
 	
-	//Connect to mysql server
-	$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
-	if(!$link) {
-		error_log('Failed to connect to server: ' . mysql_error());
-    exit;
-	}
-	
-	//Select database
-	$db = mysql_select_db(DB_DATABASE);
-	if(!$db) {
-		error_log("Unable to select database");
-    exit;
-	}
-	
 	//Function to sanitize values received from the form. 
   //Prevents SQL injection.
 	function clean($str) {
 		$str = @trim($str);
-		return mysql_real_escape_string($str);
+		return mysqli_real_escape_string($str);
+	}
+	
+	$link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+	if ( !$link ) {
+		error_log('Failed to connect to server: ' . mysqli_connect_error());
+		die( 'Connect error: (' . mysqli_connect_errno() . ') ' . mysqli_connect_error() );
+		exit; // just in case
 	}
 	
 	//Sanitize the parameter values
@@ -30,14 +23,14 @@
 	
 	//Create query
 	$qry="SELECT * FROM players WHERE login='$login' AND passwd='$password'";
-	$result=mysql_query($qry);
+	$result = mysqli_query( $link, $qry );
 
 	//Check whether the query was successful or not
 	if($result) {
-		if(mysql_num_rows($result) == 1) {
+		if(mysqli_num_rows($result) == 1) {
 			//Login Successful
 			session_regenerate_id();
-			$playerrow = mysql_fetch_assoc($result);
+			$playerrow = mysqli_fetch_assoc($result);
 			$_SESSION['SESS_PLAYER_ID'] = $playerrow['player_id'];
       if ($playerrow['firstname'] == '') {
         $firstname = $login;
