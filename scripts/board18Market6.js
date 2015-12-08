898545563492602511edbe682dcea117c9bbf666
Richard Price <rich@gandalf.ws>
2015-12-08 16:42:12 -0600

    resolve conflict in board18SnapMrk.php file

--- scripts/board18Market6.js
+++ scripts/board18Market6.js
@@ -9,6 +9,32 @@
  * A copy of this license can be found in the LICENSE.text file.
  */
 
+/* The makeTrayItems function will use the 
+ * BD18.trays array to construct the items
+ * to be displayed in the tray menu.
+ */
+function makeTrayItems() {
+  var menuText = '<ul class="leftMenu">';
+  var lastItem = BD18.trays.length - 1;
+  for (var ix = 0; ix < BD18.trays.length; ix++) {
+    menuText += '<li onclick="leftMenuEvent(\'tray' + ix + '\');">';
+    menuText += BD18.trays[ix].trayName;
+    menuText += '</li>';
+    menuText += (ix === lastItem) ? '</ul>' : '';
+  }
+  //var menuItems = $.parseJSON(menuText);
+  return menuText;
+}
+
+/* The registerTrayMenu function creates the 
+ * tray menu on the board18Map page. It uses
+ * the jquery context menu plugin and the
+ * makeTrayItems function.
+ */
+function registerTrayMenu() {
+  var itemlist = makeTrayItems();
+  $('#traymenu').html(itemlist);
+}
 
 /* This function handles the selection of the leftMenu(Tray)
  * and displays the proper BD18.tray.
