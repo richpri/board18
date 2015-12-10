fc0ce150dd87002e3a47d40707a59523d8e5bf4a
Richard Price <rich@gandalf.ws>
2015-12-09 19:28:55 -0600

    Added release notes for release 1.3.4

--- makedist/config.sh
+++ makedist/config.sh
@@ -2,7 +2,7 @@
 # BOARD18make is run to create a new BOARD18 distribution.
 
 # Set versionNo to the version number of the new distribution
-versionNo="1.3.3"
+versionNo="1.3.4"
 
 # Set BD18Root to the root of the BOARD18 directory structure.
 BD18Root=~/NetBeansProjects/BOARD18/
