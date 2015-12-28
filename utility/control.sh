92ed8c646c0ae48a4e56f75aadfc54a01fc6c933
Richard Price <rich@gandalf.ws>
2015-12-28 15:16:54 -0600

    Release 1.4.0

--- utility/control.sh
+++ utility/control.sh
@@ -11,4 +11,4 @@ BD18Site=~/prod1.board18.org
 BD18Old=~/prod1.board18.old
 
 # Set BD18New to the webroot directory in the new BOARD18 release.
-BD18New=~/board18-1.1.6/webroot
+BD18New=~/board18-1.4.0/webroot
